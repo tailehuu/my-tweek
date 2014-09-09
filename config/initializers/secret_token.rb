3c76563297505ce446ca1732baaebfc37e36b256
Tai LE <tai.le@infonam.com>
2014-09-09 15:44:37 +0700

    'init'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTweek::Application.config.secret_token = '0b30695cde5d17fb782628bbb4befcf4c14eade06b60774306979e67149de04309a525eb64c0ea81d82e84435207ba75f7f08a286b9697cd5e2ab5e68217cdea'
